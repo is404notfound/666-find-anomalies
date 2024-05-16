1984b353a7261453bafeb4e59f992d44cb9b5592
easyoon <easyoon@wemakeprice.com>
2024-05-17 02:26:23 +0900

    조이스틱에 따른 플레이어 이동 추가

--- 3d-web/src/App.tsx
+++ 3d-web/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { Canvas, useLoader } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Physics } from '@react-three/rapier';
 import WallPanel from './components/WallPanel';
 import pattern from './assets/glitch-wall.jpeg';
 import { RepeatWrapping, TextureLoader } from 'three';
-import { FirstPersonControls } from '@react-three/drei';
+import VirtualJoystick from './components/VirtualJoystick';
+import MovingControlPanel, { JoystickData } from './components/MovingControlPanel';
 
 function App() {
+  const [joystickData, setJoystickData] = useState<JoystickData>({ deltaX: 0, deltaY: 0 });
   const textureRepeat = [4, 4];
   const texture = useLoader(TextureLoader, pattern);
   texture.wrapS = RepeatWrapping;
@@ -27,21 +29,22 @@ function App() {
         }
       }
     >
+      <VirtualJoystick onMove={setJoystickData} />
       <Canvas
         shadows
-        camera={{fov: 75, far: 1000, near: 1 }}
+        camera={{ fov: 75, far: 1000, near: 1 }}
         style={{ flex: 1 }}
-        >
+      >
         <color attach="background" args={['#f0f0f0']} />
         <Suspense fallback={null}>
           <Physics gravity={[0, 1, 0]}> 1
             <ambientLight intensity={0.5} />
             <directionalLight position={[-10, 10, 0]} intensity={0.4} />
-            <FirstPersonControls movementSpeed={20} />
+            <MovingControlPanel joystickData={joystickData} />
 
             {/* Center */}
-            <WallPanel position={[0, -15, 0]} args={[80, 1, 100]} color='pink' />
-            <WallPanel position={[0, 25, 0]} args={[80, 1, 100]} color='pink' />
+            <WallPanel position={[0, -15, 0]} args={[80, 1, 100]} color='white' />
+            <WallPanel position={[0, 25, 0]} args={[80, 1, 100]} color='white' />
             <WallPanel position={[-40, 5, -40]} args={[1, 40, 180]} color='white' texture={texture} />
             <WallPanel position={[40, 5, 40]} args={[1, 40, 180]} color='white' texture={texture} />
 
@@ -50,7 +53,7 @@ function App() {
             <WallPanel position={[-50, -15, 90]} args={[180, 1, 80]} color='white' />
             <WallPanel position={[-130, 5, 50]} args={[180, 40, 1]} color='white' texture={texture} />
             <WallPanel position={[-50, 5, 130]} args={[180, 40, 1]} color='white' texture={texture} />
-            
+
             <WallPanel position={[-180, -15, 140]} args={[80, 1, 180]} color='white' />
             <WallPanel position={[-180, 25, 140]} args={[80, 1, 180]} color='white' />
             <WallPanel position={[-220, 5, 140]} args={[1, 40, 180]} color='white' texture={texture} />
