866a8c4c386f88be3f39bac1e050ab4f6903a5a8
easyoon <easyoon@wemakeprice.com>
2024-06-11 22:58:09 +0900

    버추얼스틱 제거 및 불필요한 구문 제거, 충돌 테스트 중

--- native/screens/Home.tsx
+++ native/screens/Home.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { Button, Text, View } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: 'bold',
-            marginBottom: 20,
-          }}>
-            
-          </Text>
-        <Button
-          title="Game Start"
-          onPress={() => navigation.navigate('Game')}
-        />
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          marginBottom: 20,
+        }} />
+      <Button
+        title="Game Start"
+        onPress={() => navigation.navigate('Game')}
+      />
+    </View>
+  );
+}
