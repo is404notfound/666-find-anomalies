431391a04bac02a7d1e15604c44062627b32f6cc
easyoon <easyoon@wemakeprice.com>
2024-06-11 22:56:02 +0900

    패널 컴포넌트 추가

---
+++ 3d-web/src/components/Panel.tsx
@@ -0,0 +1,50 @@
+import { Box, Text } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
+import { DoubleSide, Texture } from 'three';
+
+interface PanelProps {
+  position: [number, number, number]
+  , args: [number, number, number]
+  , color?: string
+  , texture?: Texture
+  , content?: string | number
+  , textColor: string
+  , textPosition: [number, number, number]
+  , textScale?: [number, number, number]
+}
+
+function Panel({
+  position
+  , args
+  , color
+  , texture
+  , content
+  , textColor
+  , textPosition
+  , textScale
+}: PanelProps) {
+  
+  return (
+    <RigidBody type='fixed' collisionGroups={1}>
+      <Box position={position} args={args}>
+        {texture ?
+          <meshStandardMaterial map={texture} roughness={0.75} metalness={0.5} side={DoubleSide} />
+          : <meshStandardMaterial color={color} />
+        }
+        <Text
+          position={textPosition}
+          fontSize={5}
+          color={textColor}
+          anchorX="center"
+          anchorY="middle"
+          rotation={[0, 1.5, 0]}
+          scale={textScale && textScale}
+        >
+          {content}
+        </Text>
+      </Box>
+    </RigidBody>
+  );
+}
+
+export default Panel;
